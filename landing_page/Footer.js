e63855530b70e158b5d8e806ce0df6584ac3d8be
netrathakur-pn <netraa2405@gmail.com>
2025-08-13 19:29:18 +0530

    Add files via upload

---
+++ landing_page/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+function Footer() {
+    return ( 
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <img src='images/images/lago.svg' style={{width:"50%"}}/>
+                </div>
+                <div className='col'>
+                    <p>Company</p>
+                      <a href=''>About</a><br/>
+                      <a href=''>Philosopy</a><br/>
+                      <a href=''>Press & media</a><br/>
+                      <a href=''>Careers</a><br/>
+                      <a href=''>Zerodha Cares (CSR)</a><br/>
+                      <a href=''>Zerodha.tech</a><br/>
+                      <a href=''>Open source</a><br/>
+                </div>
+                <div className='col'>
+                    <p>Support</p>
+                         <a href=''>Contact us</a><br/>
+                         <a href=''>Support portal</a><br/>
+                         <a href=''>How to file a complaint?</a><br/>
+                         <a href=''>Status of your complaints</a><br/>
+                         <a href=''>Bulletin</a><br/>
+                         <a href=''>Circular</a><br/>
+                         <a href=''>Z-Connect blog</a><br/>
+                         <a href=''>Downloads</a><br/>
+                </div>
+                <div className='col'>
+                    <p>Account</p>
+                        <a href=''>Open demat account</a><br/>
+                        <a href=''>Fund tranfer</a><br/>
+                        <a href=''>0 day challenge</a><br/>
+                
+                    
+                        
+                        
+                        
+                        
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Footer;
